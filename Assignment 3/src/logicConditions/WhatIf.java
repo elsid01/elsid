639d78f3353d7bb33d04ad3ec86ce2d6e060b541
elsid <elymbayw417@gmail.com>
2019-09-30 15:13:35 -0400

    Assignment 3 java files uploaded

---
+++ Assignment 3/src/logicConditions/WhatIf.java
@@ -0,0 +1,18 @@
+package logicConditions;
+
+import java.util.Scanner;
+
+public class WhatIf {
+
+	public static void main(String[] args) {
+		
+		// declaring variables
+		int people, cats, dogs;
+		
+		
+		
+		
+		
+	}
+
+}

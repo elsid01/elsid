8c4692698726492571d68222235c944772d210da
elsid01 <50616793+elsid01@users.noreply.github.com>
2019-10-23 16:48:50 -0400

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,16 @@
+package sbaweekfiveandsix;
+
+//implements the Shape interface
+public class Circle implements Shape {
+	public float getArea() {
+		return 0;
+	};
+	
+    public float getPerimeter() {
+    	return 0;
+    };
+	 
+    public String toString() {
+    	return "x";
+    };
+}
